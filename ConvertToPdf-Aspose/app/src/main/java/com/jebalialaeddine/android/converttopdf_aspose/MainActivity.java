a20dcb8a0d0c1ec79f7391b6440d874a8c415aea
Ala Eddine JEBALI <jebali.alaeddine@gmail.com>
2015-04-03 15:56:12 +0100

    Add Aspose (for MS-word) jar

--- ConvertToPdf-Aspose/app/src/main/java/com/jebalialaeddine/android/converttopdf_aspose/MainActivity.java
+++ ConvertToPdf-Aspose/app/src/main/java/com/jebalialaeddine/android/converttopdf_aspose/MainActivity.java
@@ -1,39 +1,62 @@
 package com.jebalialaeddine.android.converttopdf_aspose;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    final Context CONTEXT = this;
+
+    private void convertFile(String filePath, String outputPath) {
+
+    }
+
+    /**
+     * show alert dialog
+     *
+     * @param title   String
+     * @param message String
+     */
+    private void showAlertDialog(String title, String message) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(CONTEXT);
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder
+                .setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //Convert File
+        Button buttonConvert = (Button) findViewById(R.id.buttonConvert);
+        buttonConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextFilePath = (EditText) findViewById(R.id.editTextFilePath);
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + editTextFilePath.getText();
+                String outputPath = filePath.substring(0, filePath.lastIndexOf('.')) + ".pdf";
+                convertFile(filePath, outputPath);
+            }
+        });
     }
 }
--- README.md
+++ README.md
@@ -14,10 +14,10 @@ To use the library.
   - The project is "/ConvertToPdf-Aspose/"
   - Download Aspose zip from http://www.aspose.com/community/files/74/android-components/aspose.pdf-for-android/default.aspx
   - When you extract you'find:
-    - "Aspose.Words.zip" for Java offers the feature to [convert word files to PDF]. For further details, please check Convert word file to PDF Furthermore, it supports variety of document formats i.e. DOC, DOCX, ODT, RTF, EPUB etc. Please check [Supported file formats for more information].
-    - "Aspose.Cells.zip" for Java offers the feature to [convert Excel worksheets to PDF]. For further details, please visit Convert Excel files to PDf please check
-    - "Aspose.Slides.zip" for Java offers the feature to [convert PowerPoint presentations to PDF]. For further details, please check Convert Presentations to PDf
-    - "Aspose.Pdf.zip" for Java offers the feature to convert Text, Image, HTML, XML files to Pdf format.
+    - "Aspose.Words.zip" offers the feature to [convert word files to PDF]. For further details, please check Convert word file to PDF Furthermore, it supports variety of document formats i.e. DOC, DOCX, ODT, RTF, EPUB etc. Please check [Supported file formats for more information].
+    - "Aspose.Cells.zip" offers the feature to [convert Excel worksheets to PDF]. For further details, please visit Convert Excel files to PDf please check
+    - "Aspose.Slides.zip" offers the feature to [convert PowerPoint presentations to PDF]. For further details, please check Convert Presentations to PDf
+    - "Aspose.Pdf.zip" offers the feature to convert Text, Image, HTML, XML files to Pdf format.
   - Add "Aspose.Pdf.Android-x.X.jar" to your "lib" folder (with Android-Studio, you may need to right-click the lib/itextpdf-x.x.x.jar -> "Add as Library...")
   - Add permission to your AndroidManifest
   
